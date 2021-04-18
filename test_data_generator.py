12f65baa892e694812912c61e0baca1b8972168d
Alexander Prymak <primak.aa@gmail.com>
2021-04-18 15:26:02 +0300

    some cosmetic fixes

--- test_data_generator.py
+++ test_data_generator.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import time
 import random
 import requests
--- weather_app_sketch/weather_app_sketch.ino
+++ weather_app_sketch/weather_app_sketch.ino
@@ -1,61 +1,61 @@
-//YL-40 AD-DC Converter
+//YL-40 ADC
 
 #include "Arduino.h"
 #include "PCF8591.h"
 #define PCF8591_I2C_ADDRESS 0x48
 long timetodelay = 2500;
-String inputString = "";         // a String to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
+String inputString = "";
+boolean stringComplete = false;
 
 PCF8591 pcf8591(PCF8591_I2C_ADDRESS);
 
 void setup()
 {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-	Serial.begin(115200);
-	pcf8591.begin();
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
+    Serial.begin(115200);
+    pcf8591.begin();
 }
 
 void loop()
 {  
-  pcf8591.analogWrite(255);
-  if (stringComplete) {
-    if (inputString.toInt() != 0) {
-      timetodelay = inputString.toInt();  
-    } else {
-      if (inputString.equals("light_on")){
-        digitalWrite(13, HIGH);
-      } else if (inputString.equals("light_off")){
-        digitalWrite(13, LOW);
-      }
-    }    
-    inputString = "";
-    stringComplete = false;
-  }
+    pcf8591.analogWrite(255);
+    if (stringComplete) {
+        if (inputString.toInt() != 0) {
+        timetodelay = inputString.toInt();
+        } else {
+            if (inputString.equals("light_on")) {
+                digitalWrite(13, HIGH);
+            } else if (inputString.equals("light_off")) {
+                digitalWrite(13, LOW);
+            }
+        }
+        inputString = "";
+        stringComplete = false;
+    }
 
-	PCF8591::AnalogInput ai = pcf8591.analogReadAll();
-  double a = ai.ain1;
-  double r6 = (1000 * a)/(256 - a);
-  double c = r6/5150;
-  double temp = (1 / (0.003354016 + (0.000253165)*log(c))) - 273.15;
+    PCF8591::AnalogInput ai = pcf8591.analogReadAll();
+    double a = ai.ain1;
+    double r6 = (1000 * a)/(256 - a);
+    double c = r6/5150;
+    double temp = (1 / (0.003354016 + (0.000253165)*log(c))) - 273.15;
 	Serial.print("temp:");  
-  Serial.print(temp);
+    Serial.print(temp);
 	Serial.print(" bright:");
 	Serial.println(ai.ain0);
-  delay(500);
-  pcf8591.analogWrite(0);
-  delay(timetodelay);
+    delay(500);
+    pcf8591.analogWrite(0);
+    delay(timetodelay);
 }
 
 void serialEvent() {
-  while (Serial.available()) {
-    char inChar = (char)Serial.read();
-    if (inChar != '\n' && inChar != '\r') {
-      inputString += inChar;
+    while (Serial.available()) {
+        char inChar = (char)Serial.read();
+        if (inChar != '\n' && inChar != '\r') {
+            inputString += inChar;
+        }
+        if (inChar == '\n') {
+            stringComplete = true;
+        }
     }
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
-  }
 }
