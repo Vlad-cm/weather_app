b13226d2a22a058a93927a143a1ebe2ea105a5a7
Alexander Prymak <primak.aa@gmail.com>
2021-05-03 02:36:13 +0300

    implemented reconnect on close websocket. implemented scrol data in chart after update /get-data weather_data (shift firts item and push last). some design fixes.

--- static/js/mychart.js
+++ static/js/mychart.js
@@ -1,52 +1,87 @@
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
 })
-if (document.getElementById('myChart') != null)
-    $(document).ready(function() {
-        $.getJSON("/get-data", function(response) {
-            let temperature = response['temp'];
-            let humidity = response['humidity'];
-            let date = response['date'];
-            let bgclr = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'
-            ];
-            let brdrclr = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'
-            ];
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: date,
-                    datasets: [{
-                            label: '# temperature',
-                            data: temperature,
-                            backgroundColor: bgclr,
-                            borderColor: brdrclr,
-                            borderWidth: 1
-                        },
-                        {
-                            label: '# humidity',
-                            data: humidity,
-                            backgroundColor: bgclr,
-                            borderColor: brdrclr,
-                            borderWidth: 1
-                        }
-                    ]
+
+let chart = document.getElementById('myChart');
+let bgclr = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)',
+ 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'];
+let brdrclr = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)',
+ 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
+let ctx, myChart;
+
+if (chart != null)
+{
+    ctx = chart.getContext('2d');
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+            {
+                label: '# temperature',
+                data: [],
+                backgroundColor: bgclr,
+                borderColor: brdrclr,
+                borderWidth: 1
+            },
+            {
+                label: '# humidity',
+                data: [],
+                backgroundColor: bgclr,
+                borderColor: brdrclr,
+                borderWidth: 1
+            }
+            ]},
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: false
+                    }
                 },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: false
-                        }
-                    },
-                    plugins: {
-                        title: {
-                            display: true,
-                            text: 'Data for the 24h'
-                        }
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Data for the 24h'
                     }
                 }
-            });
+            }
         });
-    })
+}
+
+function removeOldestData(chart) {
+    chart.data.labels.shift();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.shift();
+    });
+    chart.update();
+}
+
+if (myChart != null)
+{
+    $.getJSON("/get-data", function(response) {
+        for (var i = 0; i < response["temp"].length; i++) {
+            myChart.data.labels.push(response["date"][i]);
+            myChart.data.datasets[0].data.push(response["temp"][i]);
+            myChart.data.datasets[1].data.push(response["humidity"][i]);
+        };
+        myChart.update();
+    });
+}
+
+
+const interval = setInterval(function() {
+    $.getJSON("/get-data", function(response) {
+        let difflabels = response["date"].filter(x => !myChart.data.labels.includes(x));
+        if (difflabels.length > 0) {
+            let difftemp = response["temp"].filter(x => !myChart.data.labels.includes(x));
+            let diffhumidity = response["humidity"].filter(x => !myChart.data.labels.includes(x));
+            for (var i = 0; i < difflabels.length; i++) {
+                removeOldestData(chart);
+                myChart.data.labels.push(difflabels[i]);
+                myChart.data.datasets[0].data.push(difftemp[i]);
+                myChart.data.datasets[1].data.push(diffhumidity[i]);
+            };
+            myChart.update();
+        }
+    });
+ }, 60000);
--- static/js/websocket.js
+++ static/js/websocket.js
@@ -3,24 +3,29 @@ let temperature = document.querySelector("#temperature"),
     heatindex = document.querySelector("#heatindex"),
     lampswitch = document.querySelector("#lampswitch"),
     usersonline = document.querySelector("#online"),
-    websocket = new WebSocket("wss://wbskt.herokuapp.com/");
+    url = "wss://wbskt.herokuapp.com/",
+    ws = new WebSocket(url);
+
 lampswitch.onclick = function(event) {
     let state = false;
     if (lampswitch.checked) {
         state = true
     }
-    websocket.send(JSON.stringify({ action: 'lampstate', lamp_on: state }));
-}
-websocket.onmessage = function(event) {
+    ws.send(JSON.stringify({ action: 'lampstate', lamp_on: state }));
+};
+
+function connect() {
+  ws = new WebSocket(url);
+  ws.onmessage = function(event) {
     data = JSON.parse(event.data);
     switch (data.type) {
         case 'weather_data':
             if (temperature != null)
-                temperature.textContent = "Temperature: " + data.data.temperature + " ℃";
+               temperature.textContent = "Temperature: " + data.data.temperature + " ℃";
             if (humidity != null)
-                humidity.textContent = "Humidity: " + data.data.humidity + " %";
+               humidity.textContent = "Humidity: " + data.data.humidity + " %";
             if (heatindex != null)
-                heatindex.textContent = "Heat index (feel like): " + data.data.heatindex + " ℃";
+               heatindex.textContent = "Heat index (feel like): " + data.data.heatindex + " ℃";
             break;
         case 'lampstate':
             lampswitch.checked = data.lamp_on;
@@ -29,7 +34,21 @@ websocket.onmessage = function(event) {
             usersonline.textContent = data.count;
             break;
         default:
-            console.error(
-                "unsupported event", data);
-    }
-};
+            console.error("unsupported event", data);
+        }
+  };
+
+  ws.onclose = function(e) {
+    console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+    setTimeout(function() {
+      connect();
+    }, 1000);
+  };
+
+  ws.onerror = function(err) {
+    console.error('Socket encountered error: ', err.message, 'Closing socket');
+    ws.close();
+  };
+}
+
+connect();
